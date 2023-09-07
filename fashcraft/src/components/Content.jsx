87a82ae567fc8ad77ebcebd3c59561699d5fcba6
Benny-Nwaro <baroh3767@aol.com>
2023-09-07 10:55:02 +0100

    Design on homepage

--- fashcraft/src/components/Content.jsx
+++ fashcraft/src/components/Content.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Content() {
-  return <div className="flex-1 w-screen h-screen text-center">Content</div>;
+  return (
+    <div className="flex flex-col  w-100 h-auto text-center border border-orange-600 pt-5 pb-6 space-y-6">
+      <div className="w-52 h-52 border border-blue-600 bottom-0 ">
+        <div className=" w-24 h-40 border border-yellow-600 bottom-0"></div>
+      </div>
+      <div className=" flex relative w-screen h-96 border border-red-600 bottom-0 bg-transparent "></div>
+      <div className="w-52 h-52 border border-yellow-600 bottom-0"></div>
+      <div className="w-52 h-52 border border-yellow-600 bottom-0"></div>
+    </div>
+  );
 }
--- fashcraft/src/components/Navbar.jsx
+++ fashcraft/src/components/Navbar.jsx
@@ -2,53 +2,33 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="bg-neutral sticky top-0">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              {/* <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              /> */}
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex items-center space-x-4 right-0 ">
-                <a
-                  href="#"
-                  className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
-                >
-                  Corporate
-                </a>
-                <a
-                  href="#"
-                  className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
-                >
-                  Services
-                </a>
-                <a
-                  href="#"
-                  className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
-                >
-                  Careers
-                </a>
-                <a
-                  href="#"
-                  className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
-                >
-                  Contact
-                </a>
-              </div>
-            </div>
-          </div>
+    <nav className="flex place-content-center bg-neutral  sticky top-0 divide-y divide-slate-200 ">
+      <div className="">
+        <div className="flex place-content-center pt-6 ">
+          <a
+            href="#"
+            className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
+          >
+            Consultancy
+          </a>
+          <a
+            href="#"
+            className="text-black hover:bg-pink-600 hover:text-white  px-3 py-2 text-sm font-medium"
+          >
+            Contact
+          </a>
         </div>
       </div>
     </nav>
