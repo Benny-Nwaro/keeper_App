361c822ac89546039ed3b6e05d3e108328abb9f4
AROH EBENEZER <baroh3767@aol.com>
2023-10-24 04:56:01 -0700

    Update designers.jsx

--- client/src/designers.jsx
+++ client/src/designers.jsx
@@ -4,47 +4,41 @@ export const designer = [
     name: "Fashion Club",
     image:
       "https://234gist.prestigenigeria.com/wp-content/uploads/2021/04/210416_113356_144.jpg",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu.
   },
 
   {
     id: 2,
     name: "BLessing Odei",
     image: "https://guardian.ng/wp-content/uploads/2018/12/Ojeih-1-2-12-18.jpg",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. 
   },
   {
     id: 3,
     name: "Native Faced Inc.",
     image:
       "https://fashioninsiders.co/wp-content/uploads/2017/12/musee-dior-granville-fashion-inspiration.png",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu.
   },
   {
     id: 4,
     name: "Fashion Club",
     image:
       "https://234gist.prestigenigeria.com/wp-content/uploads/2021/04/210416_113356_144.jpg",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu.
   },
 
   {
     id: 5,
     name: "BLessing Odei",
     image: "https://guardian.ng/wp-content/uploads/2018/12/Ojeih-1-2-12-18.jpg",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu.
   },
   {
     id: 6,
     name: "Native Faced Inc.",
     image:
       "https://fashioninsiders.co/wp-content/uploads/2017/12/musee-dior-granville-fashion-inspiration.png",
-    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. — ...
-I don't do fashion",
+    note: "You can have anything you want in life if you dress for it. Clothes mean nothing until someone lives in them. Style is a way to say who you are without having to speak. Fashion is like eating, you shouldn't stick to the same menu. 
   },
 ];
